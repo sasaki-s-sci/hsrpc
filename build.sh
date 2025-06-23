a126762239167a20d0c9037cc3e2237378862615
sasaki-s-sci <sasaki.s.sci@gmail.com>
2025-06-23 07:39:37 +0000

    fix pubspec's fields

--- build.sh
+++ build.sh
@@ -12,14 +12,14 @@ SCHEMA_DIR="$SCRIPT_DIR/schema/openapi/$VERSION/hsrpc.openapi.yaml"
 # For Dart
 OUT_DIR_DART="$OUT_DIR/dart"
 mkdir -p "$OUT_DIR_DART"
-rm -rf "$OUT_DIR_DART/lib" "$OUT_DIR_DART/test" "$OUT_DIR_DART/pubspec.yaml"
+rm -rf "$OUT_DIR_DART/lib" "$OUT_DIR_DART/test"
 cd $OUT_DIR_DART &&
   dart pub global activate openapi_generator_cli &&
   export PATH="$PATH":"$HOME/.pub-cache/bin" &&
   openapi-generator generate \
     -i $SCHEMA_DIR \
     -g dart-dio \
-    --additional-properties=pubName=hsrpc,pubVersion=$VERSION,dateLibrary=core,description="auto generated openapi client for hsrpc" \
+    --additional-properties=pubName=hsrpc,pubVersion=$VERSION,dateLibrary=core,pubDescription="auto generated openapi client for hsrpc",pubHomepage="https://github.com/sasaki-s-sci/hsrpc" \
     -o "./" &&
   dart pub get &&
   dart run build_runner build
